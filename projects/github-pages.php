80c63f2b84c00f5a9b635c2c37ebfba7a15dde9e
KhanMan37 <50220544+KhanMan37@users.noreply.github.com>
2024-05-31 23:38:05 -0400

    Wordpress install

---
+++ projects/github-pages.php
@@ -0,0 +1,17 @@
+
+<?php
+class projects {
+  // Properties
+  public $name;
+  public $description;
+
+  // Methods
+  function set_name($name) {
+    $this->name = $name;
+  }
+  function get_name() {
+    return $this->name;
+  }
+  
+}
+?>
